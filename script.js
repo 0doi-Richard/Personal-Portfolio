95bfb3b9800caa1b77721c12217100a520f10d5c
Richard Odoi <87043707+0doi-Richard@users.noreply.github.com>
2024-12-20 17:56:26 +0000

    New updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Canvas bubbles scripts
 const canvas = document.getElementById('bubbleCanvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -49,6 +50,7 @@ function initBubbles() {
 initBubbles();
 bounceBubbles();
 
+// General scripts
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
@@ -62,6 +64,28 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
     });
 
 });
